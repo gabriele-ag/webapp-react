39c3b35d025ae3dcdac161deb302a6eaef0f21d9
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-06-27 15:53:20 +0200

    style: css boostrap nav

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -15,18 +15,20 @@ const AppHeader = () => {
     return (
         
             <header>
-                <nav>
-                    <div>
-                        <ul className="">
-                        {movieLinks.map((curLink, index) => (
-                            <li className="" key={index}>
-                                <NavLink to={curLink.url}>
-                                    {curLink.title}
-                                </NavLink>
-                            </li>
-                        ))};
+                <nav className="navbar navbar-expand-lg bg-dark">
+                    <div className="container-fluid">
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                            {movieLinks.map((curLink, index) => (
+                                <li className="nav-item" key={index}>
+                                    <NavLink className="nav-link text-light" aria-current="page" to={curLink.url} href="#">
+                                        {curLink.title}
+                                    </NavLink>
+                                </li>
+                            ))}
 
-                        </ul>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
             </header>
